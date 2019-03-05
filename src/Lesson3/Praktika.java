2ef150efa037ade7e575725ee5d3004b548f1217
kegus <kegus@mail.ru>
2019-03-05 10:31:22 +0600

    Lesson3 - 2 guessNumber

--- src/Lesson3/Praktika.java
+++ src/Lesson3/Praktika.java
@@ -1,7 +1,33 @@
 package Lesson3;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Praktika {
     public static void main(String[] args) {
-        System.out.println("1");
+        guessNumber();
+    }
+    private static void guessNumber(){
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+        int n_try = 3;
+        int n_guess;
+        int rnd_num = rnd.nextInt(10);
+        while (n_try > 0){
+            System.out.println("Угадате число от 0 до 9 (есть "+n_try+" попытки)");
+            n_guess = sc.nextInt();
+            if(rnd_num > n_guess)
+                System.out.println("Загаданное число больше");
+            else if(rnd_num > n_guess)
+                System.out.println("Загаданное число меньше");
+            else {
+                System.out.println("Вы угадали");
+                break;
+            }
+            n_try--;
+        }
+        if (n_try == 0)
+            System.out.println("Вы проиграли");
+        sc.close();
     }
 }
