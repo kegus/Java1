8294981de8cf7729032034607318841b58055909
kegus <kegus@mail.ru>
2019-03-24 09:36:12 +0600

    Lesson8. Initial comit.

---
+++ src/Lesson8/Moba_GRAPH/Moba_GRAPH.java
@@ -0,0 +1,4 @@
+package Lesson8.Moba_GRAPH;
+
+public class Moba_GRAPH {
+}
---
+++ src/Lesson8/SelectGame.java
@@ -0,0 +1,7 @@
+package Lesson8;
+
+public class SelectGame {
+    public static void main(String[] args) {
+        System.out.println("Выберите игру");
+    }
+}
